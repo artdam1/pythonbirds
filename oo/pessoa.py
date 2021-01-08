e84bb1e0cfd94998214b7de1328783bd12d2dcbb
artdam <damasceno.arthur.1@gmail.com>
2021-01-08 09:57:55 -0300

    Criado atributo de classe olhos

--- oo/pessoa.py
+++ oo/pessoa.py
@@ -1,4 +1,8 @@
 class Pessoa:
+    olhos = 2   #atributo de classe, esse atributo é algo comum a todos, por isso faz mais sentido ser
+                # criado fora do __init__, já idade, nome e filhos tbm é comum porém ainda varia de acordo com cada
+                #pessoa, diferente dos olhos que em situações muito especificas podem varias.
+
     def __init__(self, *filhos, nome=None, idade=35):
         self.idade = idade           # exemplos de boas praticas
         self.nome = nome             # ok
@@ -18,11 +22,18 @@ if __name__ == '__main__':
     print(luciano.idade)
     for filhos in luciano.filhos:
         print(filhos.nome)
-    luciano.sobrenome = 'ramalho'  # usualmente não é uma boa pratica
+    luciano.sobrenome = 'ramalho'    # usualmente não é uma boa pratica, é um atributo de instância pois so pertence ao
+                                     # objeto luciano.
     del luciano.filhos
-    print(luciano.__dict__)  # o __dict__ mostra os atributos de instancias presente nos atributos dinamicos
+    luciano.olhos = 1
+    del luciano.olhos
+    print(luciano.__dict__)          # o __dict__ mostra os atributos de instancias presente nos atributos dinamicos
     print(renzo.__dict__)
-
+    Pessoa.olhos = 3
+    print(Pessoa.olhos)
+    print(luciano.olhos)
+    print(renzo.olhos)
+    print(id(Pessoa.olhos), id(luciano.olhos), id(renzo.olhos))
 
 
 
